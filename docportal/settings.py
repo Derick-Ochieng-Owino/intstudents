28f630d39d09e1856692074f2695134421416fbd
Derick Owino <derickrevived777@gmail.com>
2026-09-12 12:56:29 +0300

    hosts

--- docportal/settings.py
+++ docportal/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('DJANGO_SECRET_KEY', default='dev-only-change-me')
 DEBUG = config('DJANGO_DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='localhost,127.0.0.1,.vercel.app', cast=Csv())
 
 INSTALLED_APPS = [
     'django.contrib.admin',
